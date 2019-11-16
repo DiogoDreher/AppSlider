d2bb1e381eda8cee9c8589354d3bb5a673024cb6
Diogo Oliveira <diogo-ramalho92@hotmail.com>
2019-11-16 18:04:51 +0000

    Primeira app em iOS

--- App01/ViewController.swift
+++ App01/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var swImageView: UIImageView!
+    
+    let images = [ #imageLiteral(resourceName: "darth"), #imageLiteral(resourceName: "luke"), #imageLiteral(resourceName: "lea"), #imageLiteral(resourceName: "hansolo"), #imageLiteral(resourceName: "chewie"), #imageLiteral(resourceName: "bobba"), #imageLiteral(resourceName: "storm"), #imageLiteral(resourceName: "emperor"), #imageLiteral(resourceName: "yoda"), #imageLiteral(resourceName: "c3po"), #imageLiteral(resourceName: "r2d2") ]
+    
+    var imageNumber = 0
+    @IBAction func nextImage(_ sender: UIBarButtonItem) {
+        imageNumber += 1
+        
+        if imageNumber > 10 {
+            imageNumber = 0
+        }
+        
+        swImageView.image = images[imageNumber]
+        
+        
     }
-
+    @IBAction func prevImage(_ sender: UIBarButtonItem) {
+        imageNumber -= 1
+        
+        if imageNumber < 0 {
+            imageNumber = 10
+        }
+        
+        swImageView.image = images[imageNumber]
+        
+        
+    }
+    
 
 }
 
